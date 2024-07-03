03ef62a7a7ef869a5e8da7808590f26fb507ac96
sandhya <tech.eve27@gmail.com>
2024-07-03 18:00:07 +0530

    modification done

---
+++ src/Timer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+import Lap from './Lap'
+
+const Timer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Timer
